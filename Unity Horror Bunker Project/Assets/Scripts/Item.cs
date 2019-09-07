f58872f6767c50d863d249260b1bd18e09a0a12d
Arjay01 <pogoyarjay000@gmail.com>
2019-09-07 23:02:20 +1000

    Interaction system v1
    version 1 of the interaction system of the player mechanic.
    - now has a crosshair as a pointer to select items to collect or drag stuff around.
    - item.cs for collectables
    - drag.cs for item dragging
    - additionally i added a chase room for an upcoming enemy chase player.

---
+++ Unity Horror Bunker Project/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    void OnMouseDown()
+    {
+        if ((transform.position - player.position).magnitude < 2)
+        {
+            Debug.Log("you pick up a box!");
+            Destroy(gameObject);
+        }
+       
+    }
+}
