8141ad3061c040d5f552ca4a8cd728b54725dcba
Arjay01 <pogoyarjay000@gmail.com>
2019-09-16 11:22:52 +1000

    File commits
     - prototype Keypad Unity Meta (-)
    - InteractWithKeypad (+)
    - InteractWithKeypad.meta (+)

---
+++ Unity Horror Bunker Project/Assets/Scripts/InteractWithKeypad.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithKeypad : MonoBehaviour
+{
+    public string curPassword = "54321";
+    public string input;
+    public bool onTrigger; //checks if the player collides with the collider
+    public bool doorOpen; //checks if the door is opened
+    public bool keypadScreen;
+    public Transform doorHinge;
+    [SerializeField] private GameObject[] buttons; 
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+        keypadScreen = false;
+        input = "";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            onClickedButton();
+
+        }
+
+        if (input == curPassword)
+        {
+            doorOpen = true;
+        }
+
+        if (doorOpen)
+        {
+            var newRot = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 25);
+            doorHinge.rotation = newRot;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!doorOpen)
+        {
+            if (onTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 400, 50), "TIP: Press 'E' to push a button"); //displays the "Tip" GUI to let the player know what to do
+
+            }
+
+        }
+    }
+
+    
+
+    void onClickedButton()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 2.0f))
+        {
+            if (hit.transform != null) 
+            {
+
+                //KeypadButton keypadButtonScript;
+                //input = keypadButtonScript.buttonNumber.ToString();
+
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (hit.transform.gameObject == buttons[i])
+                    {
+                        input += i.ToString();
+                    }
+                }
+
+            }
+        }
+    }
+}
