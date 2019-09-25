17b1f8cdacd240177e3098ed39c95fa03ea55073
EneLuis <cristiluis40@gmail.com>
2019-09-25 22:32:08 +0300

    Setup Notes System Scripts

---
+++ Unity Horror Bunker Project/Assets/Scripts/NotesScripts/NotesSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+[Serializable()]
+public struct UIElements
+{
+    [SerializeField] TextMeshProUGUI textObj;
+    public TextMeshProUGUI TextObj { get { return textObj; } }
+
+    [SerializeField] TextMeshProUGUI subscriptText;
+    public TextMeshProUGUI SubscriptText { get { return subscriptText; } }
+
+    [SerializeField] CanvasGroup subscriptGroup;
+    public CanvasGroup SubscriptGroup { get { return subscriptGroup; } }
+
+    [SerializeField] Image page;
+    public Image Page { get { return page; } }
+
+    [SerializeField] CanvasGroup noteCanvasGroup;
+    public CanvasGroup NoteCanvasGroup { get { return noteCanvasGroup; } }
+
+    [SerializeField] CanvasGroup readButton;
+    public CanvasGroup ReadButton { get { return readButton; } }
+
+    [SerializeField] CanvasGroup nextButton;
+    public CanvasGroup NextButton { get { return nextButton; } }
+
+    [SerializeField] CanvasGroup previousButton;
+    public CanvasGroup PreviousButton { get { return previousButton; } }
+
+    /*[SerializeField] NoteData noteDataPrefab;
+    public NoteData NoteDataPrefab { get { return noteDataPrefab; } }*/
+
+
+
+}
+
+public class NotesSystem : MonoBehaviour
+{
+     
+}
---
+++ Unity Horror Bunker Project/Assets/Scripts/NotesScripts/Page.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PageType { Text, Texture }
+
+[CreateAssetMenu(fileName = "new Page", menuName = "Notes System/ new Page" )]
+public class Page : MonoBehaviour
+{
+    [SerializeField] PageType type = PageType.Text;
+    public PageType Type { get { return type; } }
+
+    [TextArea(8, 16)]
+    [SerializeField] string text = string.Empty;
+    public string  Text { get { return text; } }
+
+    [SerializeField] Sprite texture = null;
+    public Sprite Texture { get { return texture; } }
+
+    [SerializeField] bool useSubscript = true;
+    public bool UseSubscript { get { return useSubscript; } }
+
+    
+
+}
