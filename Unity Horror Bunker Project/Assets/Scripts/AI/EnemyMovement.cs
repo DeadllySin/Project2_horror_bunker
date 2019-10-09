fc1deeb5d240decf70737f6227a599be2c128d74
Arjay01 <pogoyarjay000@gmail.com>
2019-10-09 10:34:36 +1000

    AI development - Caretaker
    - AI as an enemy to the player
    - Patrol
    - Chase (needs some work)
    - Nav Mesh Agent Component

---
+++ Unity Horror Bunker Project/Assets/Scripts/AI/EnemyMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    NavMeshAgent Agent;
+    [SerializeField] private Transform[] wayPoints;
+    int currentWaypoint = 0;
+
+    enum EnemyState { Patrol, Chase }
+    [SerializeField] private EnemyState currentState;
+
+    [SerializeField] private Transform target;
+
+
+    private void Start()
+    {
+        // Set reference for Navmesh agent
+        Agent = GetComponentInParent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        if (currentState == EnemyState.Patrol)
+        {
+            // code executes for AI to patrol 
+            if (Vector3.Distance(transform.position, wayPoints[currentWaypoint].position) < 0.6f)
+            {
+                currentWaypoint++;
+                if (currentWaypoint == wayPoints.Length)
+                {
+                    currentWaypoint = 0;
+                }
+            }
+            Agent.SetDestination(wayPoints[currentWaypoint].position);
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (currentState == EnemyState.Chase)
+        {
+            Agent.speed = 2f;
+            Agent.SetDestination(target.position);
+        }
+
+        if (currentState == EnemyState.Patrol)
+        {
+            Agent.speed = 1f;
+            Agent.SetDestination(wayPoints[currentWaypoint].position);
+        }
+    }
+}
---
+++ Unity Horror Bunker Project/Assets/Scripts/AI/FOVdetect.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVdetect : MonoBehaviour
+{
+    public Transform player;
+    public float maxAngle;
+    public float maxRadius;
+
+    private bool isInFov = false;
+
+    private void OnDrawGizmos()
+    {
+        // To create lines of radius and angle for testing
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+
+        Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(transform.position, fovLine1);
+        Gizmos.DrawRay(transform.position, fovLine2);
+
+        if (!isInFov)
+        {
+            // if the player is NOT in FOV
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            // if the player is in FOV
+            Gizmos.color = Color.green;
+        }
+        
+        Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
+
+    }
+
+    public static bool inFOV(Transform checkingObject, Transform target, float maxAngle, float maxRadius)
+    {
+        Collider[] overLaps = new Collider[30];
+        int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overLaps);
+
+        for (int i = 0; i < count; i++)
+        {
+            if (overLaps[i] != null)
+            {
+
+                if (overLaps[i].transform == target)
+                {
+
+                    Vector3 directionBetween = (target.position - checkingObject.position).normalized;
+                    directionBetween.y *= 0;
+
+                    float angle = Vector3.Angle(checkingObject.forward, directionBetween);
+
+                    if (angle <= maxAngle)
+                    {
+
+                        Ray ray = new Ray(checkingObject.position, target.position - checkingObject.position);
+                        RaycastHit hit;
+
+                        if (Physics.Raycast(ray, out hit, maxRadius))
+                        {
+                            if (hit.transform == target)
+                            {
+                                return true;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        isInFov = inFOV(transform, player, maxAngle, maxRadius);
+    }
+}
